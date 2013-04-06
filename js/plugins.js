56d53d49eb92a468df196576ad002a295f2759ab
Kenny Jackelen <kenny@kennyjackelen.com>
2013-04-05 22:37:26 -0500

    working v1

---
+++ js/plugins.js
@@ -0,0 +1,35 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+function squarifyMe(element) {
+  squareItUp()
+  window.onresize = function(element) {
+    squareItUp();
+  }
+  function squareItUp() {
+    var newHeight = (2/3)*$(element).width();
+    $(element).height(newHeight);
+  }
+}
